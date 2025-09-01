4783db142511e6372f30db28e747b67912cdb2f4
GabrielSousaSPTech <gabriel.sousa@sptech.school>
2025-09-01 14:50:37 -0300

    Primeiro commit para a Graxa API

---
+++ src/test/java/com/Graxa_API/Graxa_API/GraxaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Graxa_API.Graxa_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraxaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
