f7048f30987688dc534e99bf9ff013389681de76
GabrielSousaSPTech <gabriel.sousa@sptech.school>
2025-09-02 18:59:55 -0300

    CRUD de usuario

--- src/main/java/com/Graxa_API/Graxa_API/Repository/UsuarioRepository.java
+++ src/main/java/com/Graxa_API/Graxa_API/Repository/UsuarioRepository.java
@@ -1,4 +1,7 @@
 package com.Graxa_API.Graxa_API.Repository;
 
-public class UsuarioRepository {
+import com.Graxa_API.Graxa_API.Model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 }
