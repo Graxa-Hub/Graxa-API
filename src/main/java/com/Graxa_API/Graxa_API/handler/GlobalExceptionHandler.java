e7018cff6108ffb0be80279e499b84dcec2390e4
GabrielSousaSPTech <gabriel.sousa@sptech.school>
2025-09-18 18:34:51 -0300

    feat:adicionando-global-exception-handler

--- src/main/java/com/Graxa_API/Graxa_API/handler/GlobalExceptionHandler.java
+++ src/main/java/com/Graxa_API/Graxa_API/handler/GlobalExceptionHandler.java
@@ -1,4 +1,22 @@
 package com.Graxa_API.Graxa_API.handler;
+import com.Graxa_API.Graxa_API.Exception.UsuarioNaoEncontradoException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(UsuarioNaoEncontradoException.class)
+    public ResponseEntity<Object> handleUsuarioNaoEncontradoException(UsuarioNaoEncontradoException e){
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("mensagem", e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
+    }
 }
