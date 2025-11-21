07c0a52532daaa946ca3e37eae94a1d5cb7b82d5
GabrielSousaSPTech <gabriel.sousa@sptech.school>
2025-11-21 19:19:45 -0300

    processo-alocacao

--- src/main/java/com/Graxa_API/Graxa_API/dto/ShowDto/ResponseShowDto.java
+++ src/main/java/com/Graxa_API/Graxa_API/dto/ShowDto/ResponseShowDto.java
@@ -5,6 +5,7 @@ import com.Graxa_API.Graxa_API.dto.BandaDto.ResponseBandaDto;
 import com.Graxa_API.Graxa_API.dto.LocalDto.ResponseLocalDto;
 import com.Graxa_API.Graxa_API.dto.TurneDto.ResponseTurneDto;
 import com.Graxa_API.Graxa_API.dto.UsuarioDto.ResponseUsuarioDto;
+import com.Graxa_API.Graxa_API.dto.AlocacaoDto.ResponseAlocacaoDto;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -18,7 +19,8 @@ public record ResponseShowDto(
         ResponseTurneDto turne,
         ResponseLocalDto local,
         ResponseUsuarioDto responsavelEvento,
-        List<ResponseBandaDto> bandas
+        List<ResponseBandaDto> bandas,
+        List<ResponseAlocacaoDto> alocacoes // ✅ novo campo
 ) {
     public static ResponseShowDto toResponse(ShowEntity entity) {
         return new ResponseShowDto(
@@ -30,7 +32,8 @@ public record ResponseShowDto(
                 ResponseTurneDto.toResponse(entity.getTurne()),
                 ResponseLocalDto.toResponse(entity.getLocal()),
                 ResponseUsuarioDto.toResponse(entity.getResponsavelEvento()),
-                ResponseBandaDto.toResponse(entity.getBandas())
+                ResponseBandaDto.toResponse(entity.getBandas()),
+                ResponseAlocacaoDto.toResponse(entity.getAlocacoes()) // ✅ converte lista
         );
     }
 
