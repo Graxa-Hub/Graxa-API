b06414781cdb7618d47c65160af859af498d3632
macari11 <macari.pinho@sptech.school>
2025-11-26 23:12:13 -0300

    feat:controller-telefone

--- src/main/java/com/Graxa_API/Graxa_API/Controller/TelefoneController.java
+++ src/main/java/com/Graxa_API/Graxa_API/Controller/TelefoneController.java
@@ -1,6 +1,7 @@
 package com.Graxa_API.Graxa_API.Controller;
 
 import com.Graxa_API.Graxa_API.Service.TelefoneService;
+import com.Graxa_API.Graxa_API.dto.TelefoneDto.RequestTelefoneDto;
 import com.Graxa_API.Graxa_API.dto.TelefoneDto.ResponseTelefoneDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -21,9 +22,60 @@ public class TelefoneController {
         this.service = service;
     }
 
-    @Operation(summary = "Lista os telefones vinculados a um usuário pelo ID", security = @SecurityRequirement(name = "BearerAuth"))
-    @GetMapping("/{id}")
-    public ResponseEntity<List<ResponseTelefoneDto>> listarTelefonesPorUsuairo(@PathVariable Long id) {
-        return service.listarPorUsuario(id);
+    @Operation(
+            summary = "Lista todos os telefones de um usuário",
+            security = @SecurityRequirement(name = "BearerAuth")
+    )
+    @GetMapping("/{usuarioId}")
+    public ResponseEntity<List<ResponseTelefoneDto>> listarTelefonesPorUsuario(
+            @PathVariable Long usuarioId
+    ) {
+        return service.listarPorUsuario(usuarioId);
+    }
+
+    @Operation(
+            summary = "Busca telefone pelo ID",
+            security = @SecurityRequirement(name = "BearerAuth")
+    )
+    @GetMapping("/telefone/{id}")
+    public ResponseEntity<ResponseTelefoneDto> buscarTelefonePorId(
+            @PathVariable Long id
+    ) {
+        return service.buscarPorId(id);
+    }
+
+    @Operation(
+            summary = "Cria telefone para usuário",
+            security = @SecurityRequirement(name = "BearerAuth")
+    )
+    @PostMapping("/{usuarioId}")
+    public ResponseEntity<ResponseTelefoneDto> criarTelefoneParaUsuario(
+            @PathVariable Long usuarioId,
+            @RequestBody RequestTelefoneDto dto
+    ) {
+        return service.criarTelefoneParaUsuario(usuarioId, dto);
+    }
+
+    @Operation(
+            summary = "Atualiza telefone",
+            security = @SecurityRequirement(name = "BearerAuth")
+    )
+    @PutMapping("/telefone/{id}")
+    public ResponseEntity<ResponseTelefoneDto> atualizarTelefone(
+            @PathVariable Long id,
+            @RequestBody RequestTelefoneDto dto
+    ) {
+        return service.atualizarTelefone(id, dto);
+    }
+
+    @Operation(
+            summary = "Remove telefone",
+            security = @SecurityRequirement(name = "BearerAuth")
+    )
+    @DeleteMapping("/telefone/{id}")
+    public ResponseEntity<Void> removerTelefone(
+            @PathVariable Long id
+    ) {
+        return service.removerTelefone(id);
     }
 }
